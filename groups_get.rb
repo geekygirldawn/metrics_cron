161330f202d36d3c15d721b4c4951d97acb0c59a
Dawn Foster <dawn@puppetlabs.com>
2012-10-27 10:06:26 -0700

    added optional argument to specify directory where output will be stored

--- groups_get.rb
+++ groups_get.rb
@@ -1,14 +1,26 @@
 #!/usr/bin/env ruby
 
+# Takes 1 argument: an existing directory name (no trailing /) 
+#    where the output will be stored.
+# If no arguments: defaults to data as local directory name
+
+if ARGV.length == 0
+   dir = "data"
+elsif ARGV.length == 1 
+   dir = ARGV[0]
+else
+   abort("Program aborted. Error: Too many arguments. One optional argument with a directory name is permitted")
+end
+
 time = Time.now
 
 date = time.strftime("%m-%d-%y:%H:%M")
 
-`wget -q -O data/puppet-users-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-users/about`
+`wget -q -O #{dir}/puppet-users-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-users/about`
 
-`wget -q -O data/puppet-dev-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-dev/about`
+`wget -q -O #{dir}/puppet-dev-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-dev/about`
 
-`wget -q -O data/puppet-razor-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-razor/about`
+`wget -q -O #{dir}/puppet-razor-#{date}.txt --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" http://groups.google.com/group/puppet-razor/about`
 
 
 
